dba6dbf7c49b0c8b90febeb341dd8c33b326ba17
Aneesa Farooq <aneesa.farooq2000@gmail.com>
2023-05-24 21:26:38 +0500

    Logo changed, app link added

--- src/components/ARModelViewer.vue
+++ src/components/ARModelViewer.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="card">
-    <model-viewer :src="modelSrc" poster="https://firebasestorage.googleapis.com/v0/b/vdesigners-5b906.appspot.com/o/images%2FLogo.png?alt=media&token=2aa9d259-3ee3-4344-ada1-22fac2f2b089" alt="v-designers logo" shadow-intensity="1" camera-controls auto-rotate ar></model-viewer>
+    <model-viewer :src="modelSrc" poster="https://firebasestorage.googleapis.com/v0/b/vdesigners-5b906.appspot.com/o/images%2Flogo-new-white.png?alt=media&token=b6b30a6a-bf07-4216-af8a-eac2f459eb37" alt="v-designers logo" shadow-intensity="1" camera-controls auto-rotate ar></model-viewer>
     <section class="attribution">
       <span>
         <h1>Augmented Reality</h1>
-        <span>By <a href="" target="_blank">V-DESIGNERs</a></span>
+        <span>By <a href="https://vdesigners-site.netlify.app/" target="_blank">V-DESIGNERs</a></span>
       </span>
       <!-- <a class="cc" href="https://creativecommons.org/licenses/by/2.0/" target="_blank">
         <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" />
